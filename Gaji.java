844ed4c47847eff7b0912aa1eae288ba10514b85
Rasyid Nugrahesa <100370094+rasyidnr@users.noreply.github.com>
2022-11-10 00:22:33 +0700

    Create Gaji.java

---
+++ Gaji.java
@@ -0,0 +1,29 @@
+public class Gaji extends Pegawai { 
+     private double salary; 
+
+     public Gaji(String name, String address, int number, double salary) { 
+         super(name, address, number); 
+         setSalary(salary); 
+     } 
+
+     public void mailCheck() { 
+//Pada method ini terjadi overridding karena mempunyai nama yang sama dengan method pada superclass sehingga terjadi upcasting
+         System.out.println("Memeriksa Kelas Gaji dalam Surat "); 
+         System.out.println("Surat Tertuju Untuk " + getName() + "dengan gaji " + salary); 
+     } 
+
+     public double getSalary() { 
+         return salary; 
+     } 
+
+     public void setSalary(double newSalary) { 
+         if (newSalary >= 0.0) { 
+             salary = newSalary; 
+         } 
+     } 
+
+     public double computerPay() { 
+         System.out.println("Menghitung Pembayaran Gaji Untuk " + getName()); 
+         return salary / 52; 
+     } 
+ }
